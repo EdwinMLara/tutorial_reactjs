63228956abfd2b0e096d3bafc7f91d88bc56485b
EdwinMLara <emlara35@gmail.com>
2020-05-14 19:59:44 -0500

    se agrego la seleccion de jugador y si existe un ganador de ser asi se ignoran los eventos

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -6,13 +6,20 @@ class Board extends React.Component{
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xisNext: true,
         }
     }
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xisNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xisNext: !this.state.xisNext,
+        });
     }
 
     renderSquare(i){
@@ -23,8 +30,16 @@ class Board extends React.Component{
             />
         );
     }
+
     render(){
-        const status = "Siguiente jugador: X";
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return(
             <div>
                 <div className="status">{status}</div>
@@ -49,3 +64,24 @@ class Board extends React.Component{
 }
 
 export default Board;
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {    
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            console.log(squares[a]);
+            return squares[a];
+        }
+    }
+    return null;
+  }
